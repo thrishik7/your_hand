2fb3a6e74e8ac5c77a0824904a21867b8d9e48f3
thrishik7 <thrishik.s@gmail.com>
2020-06-23 00:11:40 +0530

    SHR INIT

---
+++ model/Model.py
@@ -0,0 +1,150 @@
+import sys 
+import numpy as np 
+import tensorflow as tf 
+import os
+
+
+
+class DecoderType:
+    BestPath=0
+    BeamSearch=1
+    WordBeamSearch=2
+
+class Model:
+     
+    batchSize = 50
+    imgSize =(128, 32)
+    maxTextLen = 32
+
+    def __init__(self , charList, decoderType=DecoderType.BestPath, mustRestore=False, dump=False):
+
+        self.dump = dump
+        self.charList= charList
+        self.decoderType= decoderType
+        self.mustRestore= mustRestore
+        self.snapID=0
+
+        self.is_train = tf.placeholder(tf.bool, name='is_train')
+        self.inputImgs= tf.placeholder(tf.float32, shape=(None, Model.imgSize[0], Model.imgSize[1]))
+        
+        self.setupCNN()
+        self.setupRNN()
+        self.setupCTC()
+
+        self.batchesTrained=0
+        self.learningRate = tf.placeholder(tf.float32, shape=[])
+        self.update_ops= tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(self.update_ops):
+            self.optimizer= tf.train.RMSPropOptimizer(self.learningRate).minimize(self.loss)
+        
+        (self.sess, self.saver) = self.setupTF()
+
+
+
+    def setupCNN(self):
+        cnnIn4d = tf.expand_dims(input= self.inputImgs, axis= 3)
+        kernelVals= [5, 5, 3, 3, 3]
+        featureVals=[1, 32, 64, 128, 128, 255]
+        strideVals = poolVals =[(2,2), (2,2), (1,2), (1,2), (1,2)]
+        numLayers= len(strideVals)
+
+        pool = cnnIn4d
+        for i in range(numLayers):
+                kernel = tf.Variable(tf.truncated_normal([kernelVals[i], kernelVals[i], featureVals[i], featureVals[i + 1]],dtype=tf.dtypes.float32))
+                conv = tf.nn.conv2d(pool, kernel, padding='SAME',  strides=(1,1,1,1))
+                conv_norm= tf.layers.batch_normalization(conv, training=self.is_train)
+                relu= tf.nn.relu(conv_norm)
+                pool= tf.nn.max_pool(relu,(1, poolVals[i][0], poolVals[i][1], 1), (1, strideVals[i][0], strideVals[i][1], 1), 'VALID')
+
+        self.cnnOut4d= pool    
+
+    def setupRNN(self):
+
+        rnnIn3d = tf.squeeze(self.cnnOut4d, axis=[2])
+        numHidden= 255
+        cells =[tf.contrib.rnn.LSTMCell(num_units=numHidden, state_is_tuple=True) for _ in range(2)]
+        
+        stacked= tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
+
+        ((fw, bw), _) = tf.nn.bidirectional_dynamic_rnn(cell_fw=stacked, cell_bw=stacked, inputs=rnnIn3d)
+        
+        concat = tf.expand_dims(tf.concat([fw,bw], 2),2)
+
+        kernel = tf.Variable(tf.truncated_normal([1,1, numHidden*2, len(self.charList)+1],dtype=tf.dtypes.float32))
+
+        self.rnnOut3d= tf.squeeze(tf.nn.atrous_conv2d(value=concat, filters= kernel, rate=1, padding='SAME'), axis=[2])
+
+    def setupCTC(self):
+        
+            self.ctcIn3dTBC = tf.transpose(self.rnnOut3d, [1, 0, 2])
+            self.gtTexts = tf.SparseTensor(tf.placeholder(tf.int64, shape=[None, 2]) , tf.placeholder(tf.int32, [None]), tf.placeholder(tf.int64, [2]))
+            self.seqLen = tf.placeholder(tf.int32, shape=[None])
+            self.loss = tf.reduce_mean(tf.nn.ctc_loss(labels=self.gtTexts, inputs=self.ctcIn3dTBC, sequence_length=self.seqLen, ctc_merge_repeated=True))
+            self.savedCtcInput = tf.placeholder(tf.float32, shape=[Model.maxTextLen, None, len(self.charList) + 1])
+            self.lossPerElement = tf.nn.ctc_loss(labels=self.gtTexts, inputs=self.savedCtcInput, sequence_length=self.seqLen, ctc_merge_repeated=True)
+
+            
+            if self.decoderType == DecoderType.BestPath:
+                self.decoder = tf.nn.ctc_greedy_decoder(inputs=self.ctcIn3dTBC, sequence_length=self.seqLen)
+            elif self.decoderType == DecoderType.BeamSearch:
+                self.decoder = tf.nn.ctc_beam_search_decoder(inputs=self.ctcIn3dTBC, sequence_length=self.seqLen, beam_width=50, merge_repeated=False)
+            elif self.decoderType == DecoderType.WordBeamSearch:
+                word_beam_search_module = tf.load_op_library('TFWordBeamSearch.so')
+                chars = str().join(self.charList)
+                wordChars = open('../data/wordCharList.txt').read().splitlines()[0]
+                corpus = open('../data/corpus.txt').read()
+                self.decoder = word_beam_search_module.word_beam_search(tf.nn.softmax(self.ctcIn3dTBC), 50, 'Words', 0.0, corpus.encode('utf8'), chars.encode('utf8'), wordChars.encode('utf8'))
+    
+    def setupTF(self):
+
+        sess= tf.Session()
+        saver= tf.train.Saver(max_to_keep=1)
+        modelDir= '../model/'
+        latestSnapshot= tf.train.latest_checkpoint(modelDir)
+
+        if self.mustRestore and not latestSnapshot:
+           raise Exception('No saved model found in:'+ modelDir)
+
+        if latestSnapshot:
+            print('Init with stored values from '+ latestSnapshot)
+            saver.restore(sess, latestSnapshot)
+        else:
+            print('Init with new values')
+            sess.run(tf.global_variables_initializer())
+
+        return(sess, saver)
+    def save(self):
+        self.snapID+=1
+        self.saver.save(self.sess,'../model/snapshot', global_step= self.snapID)
+    
+    def inferBatch(self,batch, calcProbability=False, probabilityOfGT=False):
+
+        numBatchElements= len(batch.imgs)
+        evalRnnOutput = self.dump or calcProbability
+        evalList =[self.decoder]+([self.ctcIn3dTBC] if evalRnnOutput else [])
+        feedDict= {self.inputImgs: batch.imgs, self.seqLen : [Model.maxTextLen]*numBatchElements, self.is_train:False}
+        evalRes = self.sess.run(evalList, feedDict)
+        decoded= evalRes[0]
+        texts = self.decoderOutputToText(decoded, numBatchElements)
+        return texts
+
+    def decoderOutputToText(self, ctcOutput, batchSize):
+
+        encodedLabelStrs=[[] for i in range(batchSize)]
+        if self.decoderType == DecoderType.WordBeamSearch:
+            blank= len(self.charList)
+            for b in range(batchSize):
+                for label in ctcOutput[b]:
+                    if label== blank:
+                        break
+                    encodedLabelStrs[b].append(label)
+        
+        else:
+            decoded= ctcOutput[0][0]
+            idxDict ={b:[] for b in range(batchSize)}
+            for(idx, idx2d) in enumerate(decoded.indices):
+                label= decoded.values[idx]
+                batchElement = idx2d[0]
+                encodedLabelStrs[batchElement].append(label)
+        
+        return [str().join([self.charList[c] for c in labelStr]) for labelStr in encodedLabelStrs]
---
+++ model/main.py
@@ -0,0 +1,112 @@
+import sys
+import argparse
+import cv2
+from Model import Model, DecoderType
+from DataLoader import DataLoader, Batch
+from SamplePreprocessor import preprocess
+import editdistance
+
+class FilePaths:
+    fnCharList= '../data/charList.txt'
+    fnTrain ='../data/'
+    fnAccuracy='../data/accuracy.txt'
+    fnInfer='../data/test.png'
+    fnCorpus= '../data/corpus.txt'
+
+def main():
+    parser= argparse.ArgumentParser()
+    parser.add_argument('--train', help='train the NN', action='store_true')
+    parser.add_argument('--validate', help='validate the NN', action='store_true')
+    parser.add_argument('--beamsearch', help='use beam search instead of best path decoding', action='store_true')
+    parser.add_argument('--wordbeamsearch', help='use word beam search instead of best path decoding', action='store_true')
+
+    args = parser.parse_args() 
+    decoderType =DecoderType.BestPath
+    if args.beamsearch:
+        decoderType= DecoderType.BeamSearch
+    elif args.wordbeamsearch:
+        decoderType= DecoderType.WordBeamSearch
+    
+    if args.train or args.validate:
+
+        loader = DataLoader(FilePaths.fnTrain, Model.batchSize, Model.imgSize, Model.maxTextLen)
+        open(FilePaths.fnCharList, 'w').write(str().join(loader.charList))
+        open(FilePaths.fnCorpus,'w').write(str(' ').join(loader.trainWords+ loader.validationWords))
+        if args.train: 
+                model = Model(loader.charList, decoderType)
+                train(model, loader)
+        elif args.validate:
+                model = Model(loader.charList, decoderType, mustRestore=True)
+                validate(model, loader)
+
+def train(model,loader):        
+        epoch =0
+        bestCharErrorRate = float('inf')
+        noImprovementSince= 0
+        earlyStopping = 5
+        while True:
+            epoch += 1
+            print('Epoch:', epoch)
+            
+            print('Train NN')
+            loader.trainSet()
+            while loader.hasNext():
+                iterInfo = loader.getIteratorInfo() 
+                batch = loader.getNext()
+                loss= model.trainBatch(batch)
+                print('Batch: ', iterInfo[0],'/', iterInfo[1], 'Loss:',loss)
+                
+            charErrorRate =validate(model, loader)
+            if charErrorRate < bestCharErrorRate:
+                print('Character error rate improved, save model')
+                bestCharErrorRate=charErrorRate
+                noImprovementSince=0
+                model.save()
+                open(FilePaths.fnAccuracy,'w').write('Validation character error rate of saved model: %f%%'% (charErrorRate*100.0))
+            else :
+                print ('character error rate not improved')
+                noImprovementSince+=1
+        
+            if noImprovementSince>=earlyStopping:
+                print('No more improvement since %d epochs.Training stopped' % earlyStopping)
+                break
+
+def validate(model, loader):
+    print('Validate NN')
+    loader.validationSet()
+    numCharErr =0
+    numCharTotal =0
+    numWordOK= 0
+    numWordTotal =0
+    while loader.hasNext():
+        iterInfo = loader.getIteratorInfo()
+        print('Batch:', iterInfo[0],'/',iterInfo[1])
+        batch= loader.getNext()
+        (recognized,_) = model.inferBatch(batch)
+
+        print('Ground truth -> Recognized')
+
+        for i in range(len(recognized)):
+            numWordOK += 1 if batch.gtTexts[i] == recognized[i] else 0
+            numWordTotal += 1
+            dist = editdistance.eval(recognized[i], batch.gtTexts[i])
+            numCharErr += dist
+            numCharTotal += len(batch.gtTexts[i])
+            print('[OK]' if dist==0 else '[ERR:%d]' % dist,'"' + batch.gtTexts[i] + '"', '->', '"' + recognized[i] + '"')
+    
+        charErrorRate = numCharErr / numCharTotal
+        wordAccuracy = numWordOK / numWordTotal
+        print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate*100.0, wordAccuracy*100.0))
+        return charErrorRate
+
+
+
+def infer(model, fnImg):
+    img= preprocess(cv2.imread(fnImg, cv2.IMREAD_GRAYSCALE), Model.imgSize)
+    batch = Batch(None, [img])
+    recognized= model.inferBatch(batch, True)
+    print('Recognized:','"'+recognized[0]+'"')
+
+
+if __name__ =='__main__':
+    main() 
